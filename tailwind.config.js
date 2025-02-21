c0dfe7c8f5ad9b65e8e1e145d13002a591ec285e
Zachran Razendra <zachranraze@recodex.id>
2025-02-21 14:11:57 +0700

    Add custom color palette and background colors to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,28 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: {
+                    DEFAULT: '#FD38EC',
+                    50: '#FFF3FD',
+                    100: '#FFE6FE',
+                    200: '#FFCCFC',
+                    300: '#FFA4F5',
+                    400: '#FF6EEF',
+                    500: '#FD38EC',
+                    600: '#E118CB',
+                    700: '#BB10A5',
+                    800: '#990F86',
+                    900: '#7D126C',
+                    950: '#540047',
+                },
+                background: {
+                    light: '#f4f6f7',
+                    dark: '#23262b',
+                    sidebar: '#171717',
+                    footer: '#171717',
+                },
+            },
         },
     },
 
