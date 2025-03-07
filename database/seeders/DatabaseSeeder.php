bd2be64b439ddac5eda9ef08b7b02214f0828fde
Zachran Razendra <zachranraze@recodex.id>
2025-03-07 21:05:21 +0700

    Remove unused seeders from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,14 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersRolesAndPermissionsSeeder::class,
             HeroSectionSeeder::class,
-            CategorySeeder::class,
-            ArticleSeeder::class,
-            EventSeeder::class,
-            LibrarySeeder::class,
             CallToActionSeeder::class,
-            ProgramSeeder::class,
-            FounderSeeder::class,
-            ManagementSeeder::class,
         ]);
     }
 }
